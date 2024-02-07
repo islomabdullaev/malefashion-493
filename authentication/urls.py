ce7a75ebb8b5ab5637b04b46f37128f72257b2eb
islomabdullaev <islom.abdullaev.02@mail.ru>
2024-02-07 10:31:16 +0500

    register form part 2

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from authentication.views import signin
+from authentication.views import signin, signout, signup
 
 app_name = 'authentication'
 
 urlpatterns = [
-    path('signin/', signin, name='signin')
+    path('signin/', signin, name='signin'),
+    path('signout/', signout, name='signout'),
+    path('signup/', signup, name='signup')
 ]
